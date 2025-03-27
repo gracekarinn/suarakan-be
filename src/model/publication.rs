43367db7c514859347d04e2dcd7eb0a23d65933d
widaputri <widapk2709@gmail.com>
2025-03-27 12:40:53 +0700

    feat: enhance publication service with create, update, and fetch functionalities

--- src/model/publication.rs
+++ src/model/publication.rs
@@ -3,7 +3,7 @@ use chrono::NaiveDateTime;
 use crate::schema::publications;
 use crate::model::admin::Admin;
 
-#[derive(Queryable, Selectable, Identifiable, Associations)]
+#[derive(Queryable, Selectable, Identifiable, Associations, AsChangeset)]
 #[diesel(belongs_to(Admin, foreign_key = adminid))]
 #[diesel(table_name = publications)]
 #[diesel(primary_key(publicationid))]
@@ -21,7 +21,7 @@ pub struct Publication {
 #[diesel(table_name = publications)]
 pub struct NewPublication {
     pub title: String,
-    pub createdat: Option<NaiveDateTime>,
+    pub createdat: NaiveDateTime,
     pub updatedat: Option<NaiveDateTime>,
     pub description: Option<String>,
     pub filelink: Option<String>,
--- src/services/publication_service.rs
+++ src/services/publication_service.rs
@@ -1,17 +1,40 @@
 use diesel::prelude::*;
 use diesel::PgConnection;
-use crate::model::publication::Publication;
+use crate::model::publication::*;
 use crate::schema::publications;
 
 pub struct PublicationService;
 
 impl PublicationService {
+    pub fn create_publication(
+        conn: &mut PgConnection, 
+        new_publication: NewPublication
+    ) -> Result<Publication, diesel::result::Error> {
+        diesel::insert_into(publications::table)
+            .values(&new_publication)
+            .get_result(conn)
+    }
+
+    pub fn get_publications(conn: &mut PgConnection) -> Result<Vec<Publication>, diesel::result::Error> {
+        publications::table.load(conn)
+    }
+
+    pub fn get_publication_by_id(conn: &mut PgConnection, publication_id: i32) -> Result<Publication, diesel::result::Error> {
+        publications::table.find(publication_id).first(conn)
+    }
+
+    pub fn update_publication(
+        conn: &mut PgConnection, 
+        publication_id: i32,
+        publication: Publication
+    ) -> Result<Publication, diesel::result::Error> {
+        diesel::update(publications::table.find(publication_id))
+            .set(&publication)
+            .get_result(conn)
+    }
+
     pub fn delete_publication(conn: &mut PgConnection, publication_id: i32) -> Result<usize, diesel::result::Error> {
         diesel::delete(publications::table.find(publication_id))
             .execute(conn)
-    }
-    
-    pub fn get_publication_by_id(conn: &mut PgConnection, publication_id: i32) -> Result<Publication, diesel::result::Error> {
-        publications::table.find(publication_id).first(conn)
     }
 }
